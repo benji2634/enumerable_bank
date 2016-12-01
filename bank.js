db48166ff7c531500be986567a98c52a05c2e1e1
Tom Benham <benji26@hotmail.co.uk>
2016-12-01 15:19:19 +0000

    ran findAccountByName test

--- bank.js
+++ bank.js
@@ -3,4 +3,15 @@ var Bank = function(name){
   this.accounts = [];
 };
 
+Bank.prototype = {
+  addAccount: function(account) {
+    this.accounts.push(account);
+  },
+  findAccountByName: function(name){
+    return this.accounts.find(function(account){
+      return account.name === name;
+    });
+  }
+};
+
 module.exports = Bank;
--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -1,13 +1,18 @@
 var Bank = require("../bank");
+var Account = require('../account');
 var assert = require("assert");
 
 describe("bank", function(){
 
 var bank1;
+var account1;
+
 
 beforeEach(function(){
   bank1 = new Bank("BenjiBank");
+  account1 = new Account("Alex", "personal"); 
 });
+
 
 it("bank has name", function() {
   assert.equal("BenjiBank", bank1.name);
@@ -15,6 +20,16 @@ it("bank has name", function() {
 
 it("bank has no accounts", function(){
   assert.equal(0, bank1.accounts.length);
+});
+
+it("account can be added", function() {
+  bank1.addAccount(account1);
+  assert.equal(1, bank1.accounts.length);
+});
+
+it("find account by name", function(){
+  bank1.addAccount(account1);
+  assert.equal("Alex", bank1.findAccountByName("Alex").name);
 })
 
 });
